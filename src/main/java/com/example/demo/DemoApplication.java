a96f128e409ef2f7d2867729483165263ae6ef23
cimpoiesudanila19 <cimpoiesudanila19@mail.ru>
2023-05-30 13:07:50 +0300

    release v1

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner(StudentRepository repository) {
+		return args -> {
+			Address address = new Address(
+					"Moldova",
+					"Chishinau",
+					"ZXC"
+			);
+			Student student = new Student(
+					"Danila",
+					"Cimpoiesu",
+					"danila.cimpoiesu@mail.ru",
+					Gender.MALE,
+					address,
+					List.of("Computer science"),
+					BigDecimal.TEN,
+					LocalDateTime.now()
+			);
+
+			repository.insert(student);
+		};
+	}
+
+}
